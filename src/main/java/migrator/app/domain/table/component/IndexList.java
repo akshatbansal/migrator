7e4544cddec85dea7667362f38eff250db806319
Peter Gasparik <gasparik.pe@gmail.com>
2019-11-04 23:00:52 +0100

    events for selectin and deselecting tables

--- src/main/java/migrator/app/domain/table/component/IndexList.java
+++ src/main/java/migrator/app/domain/table/component/IndexList.java
@@ -1,7 +1,11 @@
 package migrator.app.domain.table.component;
 
+import migrator.app.domain.table.model.Index;
 import migrator.app.gui.GuiNode;
+import migrator.lib.emitter.Subscriber;
+import migrator.lib.emitter.Subscription;
 
 public interface IndexList extends GuiNode {
-    
+    public void deselect();
+    public Subscription<Index> onSelect(Subscriber<Index> subscriber);
 }
